4ebccaa651f8ee89222b758e75809ca2e01bea0f
jweboy <jweboy0630@gmail.com>
2025-03-12 23:15:44 +0800

    feat: 更新接口地址

--- constants/common.ts
+++ constants/common.ts
@@ -2,3 +2,5 @@ export const DASHBOARD = "/dashboard";
 
 export const REQUEST_BASE_URL =
   "https://global-api.jweboy.online/hms/sourcing/app-api";
+
+// export const REQUEST_BASE_URL = "http://207.180.210.128:10010/hms/sourcing/app-api";
